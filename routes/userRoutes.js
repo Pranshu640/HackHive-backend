c97700d4afb5ffbc705c897b56a32decaeb007ec
Pranshu640 <pranshubansal603@gmail.com>
2025-03-29 19:41:32 +0530

    backend setup

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.get('/profile', userController.getProfile);
+router.patch('/profile', userController.updateProfile);
+
+module.exports = router;
